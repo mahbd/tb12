dfc4beb4fb546ea6786a709cf83f9058f34b4553
mahbd <mahmudula2000@gmail.com>
2020-07-10 12:35:46 +0600

    add csrf exempt

--- bot/urls.py
+++ bot/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from bot import views
 
 app_name = 'bot'
 urlpatterns = [
-    path('get/', views.bot_get, name='get'),
+    path('get/', csrf_exempt(views.bot_get), name='get'),
 ]
