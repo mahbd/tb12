24c73b5577ff52490cb551ef9d36865882e15441
mahbd <mahmudula2000@gmail.com>
2020-07-10 10:24:24 +0600

    added last submission function codeforces

--- cf/urls.py
+++ cf/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from cf import views
 
 urlpatterns = [
-    path('', views.test, name='find'),
+    path('last_sub_cf/', views.last_submission, name='find'),
 ]
--- cf/views.py
+++ cf/views.py
@@ -3,9 +3,15 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def test(request):
-    url = 'https://codeforces.com/submissions/mah_python/page/1'
-    response = requests.get(url).content
-    page = BeautifulSoup(response, 'html.parser')
-    fn = page.findAll('tr')[26].findAll('td', attrs={'class': 'status-small'})[0].text
-    return HttpResponse(fn)
+def last_submission(request):
+    handle_list = {'mah_python': 'Mahmudul Alam','shakib112': 'Shakib Hossain Shanto',
+                    'MILOY': 'S.M. Miloy', 'nawab69':'Kibria', 'rakibnsajib1': 'Rakib Hossain Sajeeb'}
+    res = "<p>last submission details</p>"
+    for handle in handle_list:
+        url = 'https://codeforces.com/submissions/' + handle + '/page/1'
+        response = requests.get(url).content
+        page = BeautifulSoup(response, 'html.parser')
+        fn = page.findAll('tr')[26].findAll('td', attrs={'class': 'status-small'})[0].text.strip()
+        fn = fn[:12] + str(int(fn[12:14]) + 3) + fn[14:]
+        res += "<p>" + handle_list[handle] + "--->" + fn + "</p>"
+    return HttpResponse(res)
