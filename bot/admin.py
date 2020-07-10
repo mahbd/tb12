290824cbe724ee6bba42492c02f0eced241f3499
mahbd <mahmudula2000@gmail.com>
2020-07-10 15:58:02 +0600

    refined remove user

--- bot/admin.py
+++ bot/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import BotAccessInfo
+from .models import BotAccessInfo, MemberList
 
 
 admin.site.register(BotAccessInfo)
+admin.site.register(MemberList)
